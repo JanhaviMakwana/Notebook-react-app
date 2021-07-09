ddaa4e9ce823ab50aa839540893e0e7c8da2b7f1
JanhaviMakwana <janhavimakwana305@gmail.com>
2021-07-09 10:57:18 +0530

    added jwt auth

--- note-backend/app/routes/note.js
+++ note-backend/app/routes/note.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { addNote, getNotes, getNoteById, updateNote, deleteNote } = require('../controllers/notes');
-const { getCurrentUser } = require('../middleware/auth');
+const { getCurrentUser, authenticate } = require('../middleware/auth');
 const router = express.Router();
 
-router.post('/update-note/:userId/:noteId', [getCurrentUser], updateNote);
-router.get('/delete-note/:userId/:noteId', [getCurrentUser], deleteNote);
-router.get('/get-note/:userId/:noteId', [getCurrentUser], getNoteById);
-router.get('/get-notes/:userId', [getCurrentUser], getNotes);
-router.post('/add-note/:userId', [getCurrentUser], addNote);
+router.post('/update-note/:userId/:noteId', [authenticate,getCurrentUser], updateNote);
+router.get('/delete-note/:userId/:noteId', [authenticate,getCurrentUser], deleteNote);
+router.get('/get-note/:userId/:noteId', [authenticate,getCurrentUser], getNoteById);
+router.get('/get-notes/:userId', [authenticate,getCurrentUser], getNotes);
+router.post('/add-note/:userId', [authenticate,getCurrentUser], addNote);
 
 
 module.exports = router;
